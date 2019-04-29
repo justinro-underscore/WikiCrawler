a0643140271123d39e6d624bd6dac2ebc60fc9de
justinro-underscore <jcroderman@gmail.com>
2019-04-29 13:38:13 -0500

    Updates search to be more accurate

--- wikipedia_webcrawler.py
+++ wikipedia_webcrawler.py
@@ -22,17 +22,14 @@ def get_loop():
       loop.reverse()
       return loop
 
-# Gets the first links from a list of paragraphs
-def get_links_from_p(list_of_p):
-  for p in list_of_p:
-    if len(p.select("a")) > 0:
-      return p.select("a")
-
 # Goes to the page given by the url
 def goto_page(url):
   # Find the title
   raw_html = simple_get(url)
-  html = BeautifulSoup(raw_html, "html.parser")
+  try:
+    html = BeautifulSoup(raw_html, "html.parser")
+  except TypeError:
+    raise RuntimeError("Error: Page could not be parsed!")
   title = html.find("h1", attrs={"id": "firstHeading", "class": "firstHeading", "lang": "en"}).text
 
   # Check if we have found a loop
@@ -44,16 +41,22 @@ def goto_page(url):
   print(title)
   pages.append(title)
 
-  # Find the next page to go to
+  # Find all paragraphs in the article
   article = html.find("div", attrs={"class": "mw-parser-output"})
-  links = get_links_from_p(article.findAll("p", attrs={"class": ""}))
-  for link in links:
-    href = link["href"]
-    # Check if link is valid
-    if (wiki_connector in href) and \
-        (wiki_help_page not in href) and \
-        (wiki_file_page not in href):
-      return goto_page(wiki_link + href)
+  paragraphs = article.findAll(["p", "ol", "ul"], attrs={"class": ""}, recursive=False)
+  # Loop through
+  for p in paragraphs:
+    links = p.select("a")
+    if links is None:
+      raise RuntimeError("Page does not exist!")
+    for link in links:
+      href = link["href"]
+      # Check if link is valid
+      if (wiki_connector in href) and \
+          (wiki_help_page not in href) and \
+          (wiki_file_page not in href) and \
+          ("#" not in href):
+        return goto_page(wiki_link + href)
   # If we have gone through all links and they are all invalid, throw error
   raise RuntimeError("No further links!")
 
